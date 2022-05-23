08a5948231889701441e191298782cf78b39b657
HapaxHypatia <r.haskmann@gmail.com>
2022-05-23 22:01:37 +1000

    Started styling entry

--- src/components/entry.jsx
+++ src/components/entry.jsx
@@ -1,11 +1,19 @@
 import React from "react";
-import SearchFunction from "./searchFunction";
+import entry from "../data/ls_A.json";
 
 function Entry() {
 	return (
 		<div>
 			<p>entry</p>
-			<SearchFunction></SearchFunction>
+			{/*Sample entry*/}
+			<div >{entry[52].main_notes}</div>
+			<div>{entry[52].part_of_speech}</div>
+			<div>{entry[52].declension}</div>
+			<div>{entry[52].gender}</div>
+			<div className={"sense"}>{entry[52].senses[0]}</div>
+			<div className={"sense"}>{entry[52].senses[1]}</div>
+			<div className={"sense"}>{entry[52].senses[2]}</div>
+			<div className={"sense"}>{entry[52].senses[3]}</div>
 		</div>
 	);
 }
--- src/components/searchFunction.jsx
+++ src/components/searchFunction.jsx
@@ -4,11 +4,6 @@ import entry from "../data/ls_A.json"
 export default function SearchFunction () {
 	return (
 		<div>
-			<div>{entry[20].main_notes}</div>
-			<div>{entry[20].part_of_speech}</div>
-			<div>{entry[20].declension}</div>
-			<div>{entry[20].gender}</div>
-			<div>{entry[20].senses}</div>
 		</div>
 	)
 }
